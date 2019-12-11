a973b61e684926cbb5947657dbf3826e10439fa3
Michael Herman <hermanmu@gmail.com>
2019-12-11 09:59:55 -0700

    update TextSizeWidget test

--- tests/widgets/TextSizeWidget.spec.js
+++ tests/widgets/TextSizeWidget.spec.js
@@ -1,6 +1,6 @@
 /* global describe, expect, it  */
 import Vuex from "vuex";
-import { shallowMount, createLocalVue } from "@vue/test-utils";
+import { shallowMount, createLocalVue, mount } from "@vue/test-utils";
 
 import TextSizeWidget from "@/widgets/TextSizeWidget.vue";
 import scaifeWidgets from "@/store";
@@ -61,5 +61,50 @@ describe("TextSizeWidget.vue", () => {
       '<div class="text-size-widget"><textsize-stub size="xs" value="md"></textsize-stub>'
     );
     expect(store.state[MODULE_NS].readerTextSize).toBe("md");
+
+    const mountWrapper = mount(TextSizeWidget, {
+      store,
+      localVue,
+    });
+    const xsSpan = mountWrapper.findAll('span').at(0);
+    const smSpan = mountWrapper.findAll('span').at(1);
+    const mdSpan = mountWrapper.findAll('span').at(2);
+    const lgSpan = mountWrapper.findAll('span').at(3);
+    const xlSpan = mountWrapper.findAll('span').at(4);
+
+    xsSpan.trigger('click');
+    expect(store.state[MODULE_NS].readerTextSize).toBe('xs');
+    expect(store.state[MODULE_NS].readerTextWidth).toBe('normal');
+    expect(mountWrapper.html()).toContain(
+      'class="text-size-control text-xs selected">Αα</span>',
+    );
+
+    smSpan.trigger('click');
+    expect(store.state[MODULE_NS].readerTextSize).toBe('sm');
+    expect(store.state[MODULE_NS].readerTextWidth).toBe('normal');
+    expect(mountWrapper.html()).toContain(
+      'class="text-size-control text-sm selected">Αα</span>',
+    );
+
+    mdSpan.trigger('click');
+    expect(store.state[MODULE_NS].readerTextSize).toBe('md');
+    expect(store.state[MODULE_NS].readerTextWidth).toBe('normal');
+    expect(mountWrapper.html()).toContain(
+      'class="text-size-control text-md selected">Αα</span>',
+    );
+
+    lgSpan.trigger('click');
+    expect(store.state[MODULE_NS].readerTextSize).toBe('lg');
+    expect(store.state[MODULE_NS].readerTextWidth).toBe('normal');
+    expect(mountWrapper.html()).toContain(
+      'class="text-size-control text-lg selected">Αα</span>',
+    );
+
+    xlSpan.trigger('click');
+    expect(store.state[MODULE_NS].readerTextSize).toBe('xl');
+    expect(store.state[MODULE_NS].readerTextWidth).toBe('normal');
+    expect(mountWrapper.html()).toContain(
+      'class="text-size-control text-xl selected">Αα</span>',
+    );
   });
 });
