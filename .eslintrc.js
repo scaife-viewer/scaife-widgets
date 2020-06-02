3b1b9a2b0a7889d213c07a258f5e36ff9376c3aa
Jacob Wegner <jwegner@eldarion.com>
2020-06-02 04:23:54 -0500

    disable no-console and no-debugger when invoked via watch

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,15 @@
+const defaultRules = {
+  'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  'vue/script-indent': ['error', 2, { baseIndent: 1 }],
+  'max-len': [2, { code: 80, tabWidth: 2, ignoreUrls: true }],
+};
+
+if (process.env.npm_lifecycle_event === 'watch') {
+  defaultRules['no-console'] = 'off';
+  defaultRules['no-debugger'] = 'off';
+}
+
 module.exports = {
   root: true,
 
@@ -5,12 +17,7 @@ module.exports = {
     node: true,
   },
 
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'vue/script-indent': ['error', 2, { baseIndent: 1 }],
-    'max-len': [2, { code: 80, tabWidth: 2, ignoreUrls: true }],
-  },
+  rules: defaultRules,
 
   parserOptions: {
     parser: 'babel-eslint',
